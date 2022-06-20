fe7f55b03ab67fb57faf0c4cfdca87aae5d45810
Gavriel Mor <MGavrielG@Gmail.com>
2022-06-20 19:56:38 +0300

    fix to react client side routing

--- src/app.js
+++ src/app.js
@@ -17,20 +17,29 @@ const PORT = process.env.PORT || 8181;
 const userRouter = require("./users/routes/routes");
 const quoteRouter = require("./quotes/routes/routes");
 const authMiddleware = require("./helpers/authMiddleware");
+const pathToFile = path.resolve(__dirname, '../public');
+const pathToIndex = path.join(__dirname, '../public/index.html');
 
+
+app.use(express.static(pathToFile));
 app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
-
 app.use(cors());
-
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "/public")));
-
+// app.use(express.urlencoded({extended: true}));
 app.use("/api/users", userRouter);
 app.use("/api/quotes", authMiddleware, quoteRouter);
 
+app.get('/*', function (req, res) {
+  res.sendFile(pathToIndex, function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 app.listen(PORT, () =>
   console.log(
     chalk.blueBright.bold(`server run on : http://:localhost:${PORT}`)
   )
 );
+
